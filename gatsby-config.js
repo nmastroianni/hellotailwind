357e5e9e316c2577492f86d096d585cabb2f8a8f
nmastroianni <suancho@gmail.com>
2021-05-28 06:38:32 -0400

    adding plugins and transformers

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,27 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require("path")
+
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+  siteMetadata: {
+    title: "Your Site Title Goes Here",
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: path.join(__dirname, `src`, `pages`),
+      },
+      __key: `pages`,
+    },
+  ],
 }
